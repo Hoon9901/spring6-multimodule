74a2279d77235fc6a58118beaa96cae015e7eda5
Hoon9901 <shonn.dev@gmail.com>
2024-01-08 12:49:39 +0900

    init projects

---
+++ 박성훈/src/test/java/com/hoon/multimodule/MultimoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoon.multimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimoduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
